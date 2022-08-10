07f5639385cc61895cbd84234440196641b72b59
Nando <fernando.deleon@gmail.com>
2022-08-10 21:13:32 +1000

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Fernando De Leon on 10/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
